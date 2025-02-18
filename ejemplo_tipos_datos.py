d34e06967f5402b8933b1c7f6a90219d94de0115
roonny7 <83233620+roonny7@users.noreply.github.com>
2025-02-18 16:07:52 -0500

    Add files via upload

---
+++ ejemplo_tipos_datos.py
@@ -0,0 +1,18 @@
+#ejemplo tipo de datos
+
+
+#ejemplo de concatenación o suma de valores
+numero1_cadena = '10'
+print(f'Número 1 en cadena : {numero1_cadena}')
+numero2_cadena = '20'
+print(f'Número 2 en cadena : {numero2_cadena}')
+
+resultado = numero1_cadena + numero2_cadena
+print(f'Concatenacion : { resultado }')
+
+#convertimos a tipo entero
+numero1_entero = int(numero1_cadena)
+numero2_entero = int(numero2_cadena)
+resultado = numero1_entero + numero2_entero
+print(f'Suma : { resultado }')
+
---
+++ recetas_cocina.py
@@ -0,0 +1,12 @@
+print('*** Receta de cocina ***')
+nombre_receta = input('Nombre de la receta :')
+ingredientes = input('Ingredientes :')
+tiempo = int(input('Tiempo de preparación : '))
+dificultad = input('Dificultad de la receta (Fácil, Intermedia, Difícil :')
+
+#Imprimir los valores de la receta
+print('\nDatos de la receta ')
+print(f'Nombre de la receta : {nombre_receta}')
+print((f'Ingredientes : {ingredientes}'))
+print(f'Tiempo de preparación : {tiempo} minutos')
+print(f'Dificultad : {dificultad}')
