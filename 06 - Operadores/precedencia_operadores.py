504093baa9da60e8b6a53205bfbf3cd27714e88b
Roonny Ruiz <roonny7@hotmail.com>
2025-03-10 15:52:32 -0500

    Sección 6

---
+++ 06 - Operadores/precedencia_operadores.py
@@ -0,0 +1,16 @@
+# 1. ParÃ©ntesis (): Los parÃ©ntesis tienen la mayor precedencia
+# 2. Exponente **: Este operador calcula la potencia de un nÃºmero.
+# 3. Unario +, -: Estos operadores realizan operaciones unarias de positivo y negativo
+# 4. MultiplicaciÃ³n *, DivisiÃ³n /, DivisiÃ³n entera //, MÃ³dulo %
+# 5. Suma +, Resta -: Estos operadores realizan operaciones aritmÃ©ticas.
+# 6. Comparaciones (==, !=, >, <, >=, <=)
+# 7. Operadores lÃ³gicos not, and, or
+# 8. AsignaciÃ³n (=, +=, -=, *=, /=, entre otros)
+
+# Ejemplo de precedencia de operadores
+# 1. Division 12 / 3 = 4
+# 2. Multiplicacion 2 * 3 = 6
+# 3. Suma 4 + 6 = 10
+# 4. Resta 10 - 1 = 9
+resultado = 12 / 3 + 2 * 3 - 1
+print(f'Resultado: {resultado}')
