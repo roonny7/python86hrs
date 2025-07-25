9a12ee05bdf3dc65e92ea18580da06d8668b8187
Roonny Ruiz <roonny7@hotmail.com>
2025-07-25 15:11:03 -0500

    Sección 8. Ciclos

--- EntradaDatosConsola/recetas_cocina.py
+++ EntradaDatosConsola/recetas_cocina.py
@@ -9,4 +9,4 @@ print('\nDatos de la receta ')
 print(f'Nombre de la receta : {nombre_receta}')
 print((f'Ingredientes : {ingredientes}'))
 print(f'Tiempo de preparación : {tiempo} minutos')
-print(f'Dificultad : {dificultad}');
+print(f'Dificultad : {dificultad}')
